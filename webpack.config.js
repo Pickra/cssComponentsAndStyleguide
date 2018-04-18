3dace0b4988f7a0ab68f181f444a0aacf8639c9a
Pickra <picker10024@yahoo.com>
2018-04-17 22:14:05 -0400

    Upgraded to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const autoprefixer = require("autoprefixer");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const StyleLintPlugin = require("stylelint-webpack-plugin");
 
 const svgs = {
@@ -16,49 +16,50 @@ const browsers = [ "Edge >= 1", "ie >= 11", "last 2 versions" ];
 const styles = isDev => {
     return {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-            use: [
-                {
-                    loader: 'css-loader',
-                    options: { sourceMap: isDev }
-                },
-                {
-                    loader: "postcss-loader",
-                    options: {
-                        sourceMap: isDev,
-                        plugins: () => [
-                            autoprefixer({ browsers: browsers })
-                        ]
-                    }
-                },
-                {
-                    loader: 'sass-loader',
-                    options: {
-                        outputStyle: isDev ? "expanded" : "compressed",
-                        sourceMap: isDev
-                    }
+        use: [
+            MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: { sourceMap: isDev }
+            },
+            {
+                loader: "postcss-loader",
+                options: {
+                    sourceMap: isDev,
+                    plugins: () => [
+                        autoprefixer({ browsers: browsers })
+                    ]
                 }
-            ]
-        })
+            },
+            {
+                loader: 'sass-loader',
+                options: {
+                    outputStyle: isDev ? "expanded" : "compressed",
+                    sourceMap: isDev
+                }
+            }
+        ]
     }
 };
 
 module.exports = (env = {dev: false}) => {
+    const isDev = env.dev;
+
     return {
         entry: {
             "css-components": "./src/entry.js",
             "../styleguide/kss-styles": "./styleguide-builder/kss.scss"
         },
+        mode: isDev ? "development" : "production",
         output: {
             path: path.join(__dirname, "dist"),
             filename: "[name].js"
         },
         module: {
-            rules: [svgs, styles(env.dev)]
+            rules: [svgs, styles(isDev)]
         },
         plugins: [
-            new ExtractTextPlugin("[name].css"),
+            new MiniCssExtractPlugin({ filename: "[name].css" }),
             new StyleLintPlugin({ configFile: "stylelint.config.js" })
         ]
     };
