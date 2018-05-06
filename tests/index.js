4eb30198dac4d43bb4eb5c745b53ae8bbb0d82c3
Pickra <picker10024@yahoo.com>
2018-05-06 16:28:26 -0400

    Added testHoverdColorContrast test to index

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,20 @@
 module.exports = {
-    "Test Index Page Accessibility": function(browser) {
+    beforeEach: browser => {
         browser
             .url(`${browser.launchUrl}index.html`)
-            .injectAxe()
+            .injectAxe();
+    },
+
+    "Index page accessibility": browser => {
+        browser
             .runAxe()
+            .end();
+    },
+    "Index sidebar hovered link color-contrast": browser => {
+        const firstSidebarLink = ".layout__nav-item:first-of-type .layout__nav-link";
+
+        browser
+            .testHoverdColorContrast(firstSidebarLink)
             .end();
     }
 };
