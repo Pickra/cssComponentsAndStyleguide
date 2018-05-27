e5b9e92dcefa8e9015b7c8d179a5b2b771046138
Pickra <picker10024@yahoo.com>
2018-05-26 21:28:38 -0400

    Added breadcrumbs

---
+++ tests/components/breadcrumbs.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "Breadcrumbs hovered color-contrast": browser => {
+        const breadcrumbs = "#breadcrumbs";
+        
+        browser
+            .url(`${browser.launchUrl}section-components.html`)
+            .injectAxe()
+            .testHoverdColorContrast(breadcrumbs)
+            .end();
+    },
+};
