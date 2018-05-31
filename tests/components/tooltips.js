16efd94307914424877955f400001986e255f12b
Pickra <picker10024@yahoo.com>
2018-05-30 22:06:26 -0400

    Added tooltips and tests

---
+++ tests/components/tooltips.js
@@ -0,0 +1,17 @@
+
+
+module.exports = {
+    "Tooltip active accessibility": browser => {
+        const container = "#kssref-components-tooltips .kss-modifier__default-name + .kss-modifier__example";
+        const defaultSibling = `${container} .tooltip-sibling`;
+        const toolTip = `${container} .tooltip`;
+
+        browser
+            .url(`${browser.launchUrl}section-components.html`)
+            .injectAxe()
+            .moveToElement(defaultSibling, 10, 10)
+            .waitForElementVisible(toolTip)
+            .runAxe(toolTip)
+            .end();
+    }
+};
