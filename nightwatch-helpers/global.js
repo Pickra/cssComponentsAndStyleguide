1a79c9943dc7f51ea48de013318e18e5a34a327b
Pickra <picker10024@yahoo.com>
2018-06-03 14:52:00 -0400

    chromedrver closes when tests end, even when there's a fail

--- nightwatch-helpers/global.js
+++ nightwatch-helpers/global.js
@@ -1,10 +1,10 @@
-const chromeDriver = require("chromedriver");
+// https://github.com/nightwatchjs/nightwatch/blob/master/examples/globalsModule.js
 
 module.exports = {
     waitForConditionTimeout: 5000,
 
-    after: function(done) {
-        chromeDriver.stop();
-        done();
+    afterEach: function(browser, done) {
+        // https://github.com/nightwatchjs/nightwatch/issues/597#issuecomment-376332346
+        browser.end(function() { done(); });
     }
 }
